b19d52ed961a28ad0526363c3bc464859422bbd4
brainburster <brainburster@outlook.com>
2021-08-24 08:10:21 +0800

    保存一下

--- test/sph/main.js
+++ test/sph/main.js
@@ -3,20 +3,110 @@ class Vec2 {
     this.x = x;
     this.y = y;
   }
-  //...
+
+  copy(){
+    return new Vec2(this.x, this.y);
+  }
+
+  norm1() {
+    return Math.abs(this.x) + Math.abs(this.y);
+  }
+
+  norm2() {
+    return (this.x ** 2 + this.y ** 2) ** 0.5;
+  }
+
+  norm2_2() {
+    return this.x ** 2 + this.y ** 2;
+  }
+
+  dot(other){
+    return this.x * other.x + this.y * other.y;
+  }
+
+  cross(other){
+    return this.x * other.y - other.x * this.y;
+  }
+
+  op_dot(other){
+    return this.x(other.x) + this.y(other.y);
+  }
+
+  op_cross(other){
+    return this.x(other.y) - this.y(other.x);
+  }
+
+  op_mul(other){
+    return new Vec2(this.x(other.x), this.y(other.y));
+  }
+
+  add(other) {
+    return new Vec2(this.x + other.x, this.y + other.y);
+  }
+
+  sub(other) {
+    return new Vec2(this.x - other.x, this.y - other.y);
+  }
+
+  mul(other) {
+    return new Vec2(this.x * other.x, this.y * other.y);
+  }
+
+  div(other) {
+    return new Vec2(this.x / other.x, this.y / other.y);
+  }
+
+  mod(other){
+    return new Vec2(this.x % other.x, this.y % other.y);
+  }
+
+  mul_s(rhs) {
+    return new Vec2(this.x * rhs, this.y * rhs);
+  }
+
+  div_s(rhs) {
+    return new Vec2(this.x / rhs, this.y / rhs);
+  }
+
+  mod_s(rhs) {
+    return new Vec2(this.x % rhs, this.y % rhs);
+  }
 }
 
 class Particle {
   constructor(x, y) {
-    this.categray = categray;
+    this.x = new Vec2(x, y);
+    //...
   }
 }
 
 class Grid {
   constructor(w, h) {
-    //...
+    this.w = w;
+    this.h = h;
+    this.data = new Array(w*h);
   }
-  add(x, y, particle) {}
+
+  clear(){
+    this.data = new Array(this.w * this.h);
+  }
+
+  check(x, y) {
+    return x >= 0 && x < this.w && y >= 0 && y < this.h;
+  }
+
+  set(x, y, particle) {
+    if(!this.check(x,y)) return false;
+    const index = x + w * y;
+    this.data[index] = particle;
+    return true;
+  }
+
+  get(){
+    if(!this.check(x,y)) return null;
+    const index = x + w * y;
+    return this.data[index]
+  }
 }
 
 class SPH {
